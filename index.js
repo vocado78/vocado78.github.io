faec4be0ab16ac112fde5f075fb5d616152289f3
vocado78 <hedeliningrid@gmail.com>
2019-07-18 12:17:09 -0700

    minor JS fixes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const callback = () => {
     sectionTops.forEach((top, i) => mapTopToLink[top] = navLinks[i]);
 
     sectionTops.forEach((top) => {
-      if (scrollPosition >= (top - 50)) { // what is 50??? no arbitrary nums
+      if (scrollPosition >= top) {
         let navLink = mapTopToLink[top];
         if (!navLink.classList.contains("active")) {
           navLinks.forEach(navLink => navLink.classList.remove("active"));
@@ -24,27 +24,27 @@ const callback = () => {
   };
 
   const menuChange = () => {
-    const tagLineTop = document.getElementsByClassName("banner-text")[0].offsetTop;
+    const bannerTextTop = document.getElementsByClassName("banner-text")[0].offsetTop;
     const currScrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
     let menu = document.getElementsByClassName("menu")[0];
 
-    if (currScrollTop > tagLineTop) {
+    if (currScrollTop > bannerTextTop) {
       menu.style.top = "-100%";
     }
 
-    if (currScrollTop < lastScrollTop && currScrollTop > tagLineTop) {
+    if (currScrollTop < lastScrollTop && currScrollTop > bannerTextTop) {
       menu.style.top = "0";
       menu.style.backgroundColor = "#1a1a1a";
     }
 
-    if (currScrollTop < lastScrollTop && currScrollTop < tagLineTop) {
+    if (currScrollTop < lastScrollTop && currScrollTop < bannerTextTop) {
       menu.style.top = "0";
       menu.style.backgroundColor = "transparent";
     }
     lastScrollTop = currScrollTop;
   };
 
-  const isScrolledIntoView = () => {
+  const scrolledIntoView = () => {
     const skills = Array.from(document.getElementsByClassName("skills-item"));
 
     skills.forEach((skill, i) => {
@@ -61,13 +61,13 @@ const callback = () => {
   document.body.onscroll = () => {
     activeLink();
     menuChange();
-    isScrolledIntoView();
+    scrolledIntoView();
   };
 
   form.addEventListener("submit", (e) => {
     const name = document.getElementById("name");
     const email = document.getElementById("email");
-    const message = document.getElementById("message");
+    const msg = document.getElementById("message");
     const errorName = document.getElementsByClassName("error-name")[0];
     const errorEmail = document.getElementsByClassName("error-email")[0];
     const errorMsg = document.getElementsByClassName("error-msg")[0];
@@ -81,7 +81,7 @@ const callback = () => {
       errorEmail.innerHTML = "Please enter your e-mail address. The email field is required and must contain a valid e-mail address.";
       e.preventDefault();
     }
-    if (message.value.length === 0 || message.value.length > 751) {
+    if (msg.value.length === 0 || msg.value.length > 751) {
       errorMsg.innerHTML = "Please write a message. The message field is required and can have a maximum of 750 characters.";
       e.preventDefault();
     }
